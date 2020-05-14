4f1b53068ba91715cd3a2d1f24820a568ef74f5b
Avi <avidrucker@gmail.com>
2020-05-14 13:23:19 -0400

    feat placeMark, gameTuple

--- index.ts
+++ index.ts
@@ -5,34 +5,34 @@
 type Mark = 'X' | 'O' | ' ';
 
 class Maybe<T> {
-  private constructor(private value: T | null) {}
+	private constructor(private value: T | null) {}
 
-  static some<T>(value: T) {
-    if (!value) {
-      throw Error('Provided value must not be empty');
-    }
-    return new Maybe(value);
-  }
+	static some<T>(value: T) {
+		if (!value) {
+			throw Error('Provided value must not be empty');
+		}
+		return new Maybe(value);
+	}
 
-  static none<T>() {
-    return new Maybe<T>(null);
-  }
+	static none<T>() {
+		return new Maybe<T>(null);
+	}
 
-  static fromValue<T>(value: T) {
-    return value ? Maybe.some(value) : Maybe.none<T>();
-  }
+	static fromValue<T>(value: T) {
+		return value ? Maybe.some(value) : Maybe.none<T>();
+	}
 
-  getOrElse(defaultValue: T) {
-    return this.value === null ? defaultValue : this.value;
-  }
+	getOrElse(defaultValue: T) {
+		return this.value === null ? defaultValue : this.value;
+	}
 
-  map<R>(f: (wrapped: T) => R): Maybe<R> {
-    if (this.value === null) {
-      return Maybe.none<R>();
-    } else {
-      return Maybe.fromValue(f(this.value));
-    }
-  }
+	map<R>(f: (wrapped: T) => R): Maybe<R> {
+		if (this.value === null) {
+			return Maybe.none<R>();
+		} else {
+			return Maybe.fromValue(f(this.value));
+		}
+	}
 }
 
 // interleaves spaces & newlines to format tic tac toe board
@@ -48,6 +48,20 @@ const formatTile = (x: Mark, i: number): string => `[${x}]${spaceOrNewLine(i)}`;
 const formatBoard = (b: Mark[]): string => b.map(formatTile).join('');
 const printString = (s: string): void => console.log(s);
 
+// to-do: confirm that this returns the correct mark for
+// whomever you decide should go first (X's or O's)
+// t == turn number
+const markFromTurn = (t: number) => (t % 2 == 0 ? 'X' : 'O');
+
+// i == index on the board where 0 is the upper left corner, 8 is the bottom right
+// m == current player's mark
+// input is prev board state, and output is new/current board state
+// to-do: reassess later that the parameter ordering should change, if you deem it necessary
+const placeMark = (b: Mark[], i: number, m: Mark): Mark[] => {
+	b[i] = m;
+	return b;
+};
+
 // gets user choice (1-9)
 // const getUserChoice = () =>
 
@@ -57,18 +71,46 @@ const printString = (s: string): void => console.log(s);
 // returns array of valid moves
 // const getValidMoves = () =>
 
+type GameTuple = [Mark[], number];
+
+const initGame = (): GameTuple => {
+	return [createNewBoard(), 0]
+}
+
+// const show = (b: Mark[], t: turn): void => {
+// }
+
 const main = () => {
-  // to-do: clean this up, so, that eventually, it looks more like
-  // start() => show() => do()
-  // createNewBoard() => formatBoard() => printString();
-  printString(formatBoard(createNewBoard()));
+	// to-do: clean this up, so, that eventually, it looks more like
+	// start() => show() => do()
+	// createNewBoard() => formatBoard() => printString();
+	// printString(formatBoard(createNewBoard()));
+	// printString(formatBoard(placeMark(createNewBoard(), 2, 'X'))); // testing - put down a single mark
+	// printString(formatBoard(placeMark(placeMark(createNewBoard(), 2, 'X'), 2, 'O'))); // testing2 - modify stored marks
+	printString(formatBoard(createNewBoard()));
 };
+
+// The [program menu] game flow is simply a recursive function (instead of a loop)
+// that ends on the base case where the game is evaluated "game over"
+// [& the player(s) choose to not play]
+// Game Flow: START state change/mutation ->
+// 		SHOW presentation update ->
+//			render board
+//			PROMPT: ask user for user input
+//		RECEIVE user input ->
+//		DO user action (process user input) 
 
 main();
 
 // interface Array<T> {
 // 	fill(value: T): Array<T>;
 // }
+
+// necessary game state
+// - whose turn it is, could be an integer,
+//   could be a boolean flag, could be derived from the board state
+// - the board array
+// To keep it all together, we can use a tuple, a union type, or an object (map)
 
 // done list:
 // build the typescript project
@@ -77,11 +119,12 @@ main();
 // create the board
 // render the board
 // start the program
+// place marks
+// decide whether to make the program coded sync or async: async w/ promises
+// establish over-arching control flow (also w/ Maybe)
 
 // to-do list:
-// place marks
 // create I/O (take user input, return back response) w/ Maybe
-// establish control flow (also w/ Maybe)
 
 // intentions:
 // use as much functional programming as possible while comprehensible
@@ -92,3 +135,8 @@ main();
 //   takeWhile(array, i=> (i+1) % 3 === 0).then(format).then(addNewLine);
 //   partitionBy(array, 3).then(format).then(addNewLine);
 // use only pure functions (no side effects)
+
+// ideas:
+// - build according to control flow (or happy path of user)
+// - build modularly from/to their logical functional conclusion
+// - async can be async/await, callbacks, promises, generators (think in streams)
